f7442b96850a2c6bb5b5e75eeac4c7c6eb06e8de
Luma Brandão <142506658+lumab23@users.noreply.github.com>
2024-10-07 20:25:54 -0300

    Delete app/src/main/java/com/aula/literatiapp/navigation/BackButtonHandler.kt
    deleted file

--- app/src/main/java/com/aula/literatiapp/navigation/BackButtonHandler.kt
+++
@@ -1,9 +0,0 @@
-package com.aula.literatiapp.navigation
-
-import android.window.OnBackAnimationCallback
-import androidx.activity.OnBackPressedDispatcherOwner
-import androidx.compose.runtime.staticCompositionLocalOf
-
-private val LocalBackPressedDispatcher =
-    staticCompositionLocalOf<OnBackPressedDispatcherOwner?> { null }
-
