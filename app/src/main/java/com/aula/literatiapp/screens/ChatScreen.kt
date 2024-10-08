3bb135c0acfdadd9461289264d000b605a68953b
Sofya Oliveira <146602186+Sofyaolvs@users.noreply.github.com>
2024-10-08 19:10:15 -0300

    Update ChatScreen.kt

--- app/src/main/java/com/aula/literatiapp/screens/ChatScreen.kt
+++ app/src/main/java/com/aula/literatiapp/screens/ChatScreen.kt
@@ -1,2 +1,68 @@
 package com.aula.literatiapp.screens
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import com.aula.literatiapp.R
+import java.lang.reflect.Modifier
+
+
+data class Message(val author:String, val body:String)
+
+
+
+@Composable
+fun MessageCard(msg: Message) {
+    Row(modifier = Modifier.padding(all = 8.dp)) {
+        Image(
+            painter = painterResource(R.drawable.it),
+            contentDescription = null,
+            modifier = Modifier
+                .size(40.dp)
+                .clip(CircleShape)
+                .border(1.5.dp, MaterialTheme.colorScheme.primary, CircleShape)
+        )
+        Spacer(modifier = Modifier.width(8.dp))
+
+        Column {
+            Text(
+                text = msg.author,
+                color = MaterialTheme.colorScheme.secondary,
+                style = MaterialTheme.typography.titleSmall
+            )
+
+            Spacer(modifier = Modifier.height(4.dp))
+
+            Surface(shape = MaterialTheme.shapes.medium, shadowElevation = 1.dp) {
+                Text(
+                    text = msg.body,
+                    modifier = Modifier.padding(all = 4.dp),
+                    style = MaterialTheme.typography.bodyMedium
+                )
+            }
+        }
+    }
+}
+
+
+
+
+@Preview
+@Composable
+fun PreviewMessageCard(){
+    ComposeTheme{
+        Surface {
+            MessageCard(
+                msg= Message("Maria", "Oii, Gostei da sua review!!")
+            )
+        }
+    }
+
+}
+
 
